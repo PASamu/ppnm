298103ce22191ef3229526ddb5d35d1845094aad
petros <petrossamu@hotmail.com>
2021-06-07 12:09:19 +0200

    Initial commit

---
+++ exercises/komplex/main.c
@@ -0,0 +1,15 @@
+#include"komplex.h"
+#include"stdio.h"
+#define TINY 1e-6
+
+int main(){
+	komplex a = {2,3}, b = {3,4};
+
+	printf("testing komplex_add..\n");
+	komplex r = komplex_add(a,b);
+	komplex R = {5,7};
+	komplex_print("a=",a);
+	komplex_print("b=",b);
+	komplex_print("a+b should   = ", R);
+	komplex_print("a+b actually = ", r);
+}
