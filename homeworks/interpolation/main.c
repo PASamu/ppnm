130bf2fdad6dcf5287ead8d4a75c13f17dc386ba
petros <petrossamu@hotmail.com>
2021-06-27 19:26:14 +0200

    Initial commit

--- homeworks/interpolation/main.c
+++ homeworks/interpolation/main.c
@@ -13,28 +13,23 @@ int main(){
 	double x[n],y[n];
 	for(int i=0;i<n;i++){
 		x[i]=0.5+i;
-		y[i]=0.2*i+RND;
+		y[i]=0.3*i+RND;
 		printf("%g %g\n",x[i],y[i]);
 	}
-	FILE* point=fopen("points.txt","w");
-	for(int i=0;i<n;i++){
-		fprintf(point,"%10.4g %10.4g\n",x[i],y[i]);
-	}
+	printf("\n\n");
+
 	gsl_interp* gsl_linterp = gsl_interp_alloc(gsl_interp_linear,n);
 	gsl_interp_init(gsl_linterp,x,y,n);
 
-	int N=100;
-	double epsilon=8.0/N;
-	double z=0;
-	for(int i=0;i<=N;i++){
+	double dz=0.1;
+	for(double z=x[0];z<=x[n-1];z+=dz){
 		double yi=linterp(n,x,y,z);
 		double yi_i=linterp_integ(n,x,y,z);
 
 		double yiGSL=gsl_interp_eval(gsl_linterp,x,y,z,NULL);
 		double yiGSL_i=gsl_interp_eval_integ(gsl_linterp,x,y,x[0],z,NULL);
 
-		printf("%10.4f %10.4f %10.4f %10.4f %10.4f\n",z,yi,yi_i,yiGSL,yiGSL_i);
-		z=z+epsilon;
+		printf("%g %g %g %g %g\n",z,yi,yi_i,yiGSL,yiGSL_i);
 	}
 	gsl_interp_free(gsl_linterp);
 return 0;
